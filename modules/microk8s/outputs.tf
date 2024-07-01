4bc244893add3121953bd2c9fd023a61e9a7cbd1
Haresh <hareshp@50gramx.io>
2024-07-01 10:58:43 +0530

    updated outputs.tf

--- modules/microk8s/outputs.tf
+++ modules/microk8s/outputs.tf
@@ -8,7 +8,7 @@ output "ethos_control_plane_dns" {
 }
 
 output "ethos_control_plane_connection_string" {
-  value = "'ssh -i ${var.ssh_private_key_file} ubuntu@${aws_eip.ethos_control_place_eip.public_dns}'"
+  value = "'ssh -i ${var.ssh_private_key_file} ec2-user@${aws_eip.ethos_control_place_eip.public_dns}'"
 }
 
 output "ethos_worker_node_public_ip" {
@@ -20,5 +20,5 @@ output "ethos_worker_node_dns" {
 }
 
 output "ethos_worker_node_connection_string" {
-  value = "'ssh -i ${var.ssh_private_key_file} ubuntu@${aws_eip.ethos_worker_node1_eip.public_dns}'"
+  value = "'ssh -i ${var.ssh_private_key_file} ec2-user@${aws_eip.ethos_worker_node1_eip.public_dns}'"
 }
